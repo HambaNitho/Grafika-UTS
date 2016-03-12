06b2d1e2cf8fe0e3ffd303e9e409262bb90e4e8c
Rahman Adianto <rahman.adianto@gmail.com>
2016-03-12 11:04:16 +0700

    delete drawer, add multiplication

---
+++ src/multiplication.hpp
@@ -0,0 +1,14 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+
+#include <vector>
+
+class multiplication {
+
+public:
+  static std::vector<std::vector<double> > result(const std::vector<std::vector<double> >& A,
+                                 const std::vector<std::vector<double> >& B);
+
+};
+
+#endif
