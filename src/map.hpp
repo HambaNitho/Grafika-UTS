5e6a24b4a34141fc563191d8a5ce5d325b550d4a
NithoAlif <nithoalif@yahoo.com>
2016-03-20 15:51:12 +0700

    integrated map

--- src/map.hpp
+++ src/map.hpp
@@ -5,16 +5,22 @@
  #ifndef _MAP_H
  #define _MAP_H
 
- #include <iostream>
- #include "canvas.hpp"
- #include "polygon.hpp"
- #include "bezier.hpp"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include "canvas.hpp"
+#include "polygon.hpp"
+#include "bezier.hpp"
 
  class map {
 	 public:
 	 	map();
+	 	void map_add_point(std::string filename, polygon &p);
+	 	void map_add_point_bezier(std::string filename, std::vector<point> &p);
 	 	void draw_map();
-	 	void draw_contour();
 	 	void set_scale(int n_scale);
 
 	 private:
